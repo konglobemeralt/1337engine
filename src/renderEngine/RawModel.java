7cce78f07829b2e0349f71071d010bb2385f92a8
konglobemeralt <jesper.blidkvist@live.se>
2017-03-11 11:50:12 +0100

    ADDED Loader.java to create VBO and VBA buffers for models

---
+++ src/renderEngine/RawModel.java
@@ -0,0 +1,27 @@
+package renderEngine;
+
+/**
+ * Created by Jesper on 2017-03-11.
+ */
+public class RawModel {
+
+    private int vaoID;
+    private int vertexCount;
+
+    public RawModel(int vaoID, int vertexCount){
+
+        this.vaoID = vaoID;
+        this.vertexCount = vertexCount;
+    }
+
+    public int getVaoID() {
+        return vaoID;
+    }
+
+    public int getVertexCount(){
+        return vertexCount;
+    }
+
+
+}
+
