d2d2d180d7a883a8d0493f7e2464bd5acc5d8629
konglobemeralt <jesper.blidkvist@live.se>
2017-04-05 17:56:24 +0200

    fixed bug in StaticShader.java

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -15,7 +15,6 @@ public class StaticShader extends ShaderProgram {
 
     private static final int MAX_LIGHTS = 4;
 
-
     private static final String VERTEX_FILE = "src/shaders/vertexShader";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader";
 
@@ -31,16 +30,14 @@ public class StaticShader extends ShaderProgram {
     private int location_numberOfRows;
     private int location_offset;
 
-
-    public StaticShader(){
+    public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
-
 
     @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
-        super.bindAttribute(1, "textureCoords");
+        super.bindAttribute(1, "textureCoordinates");
         super.bindAttribute(2, "normal");
     }
 
@@ -58,38 +55,39 @@ public class StaticShader extends ShaderProgram {
 
         location_lightPosition = new int[MAX_LIGHTS];
         location_lightColour = new int[MAX_LIGHTS];
-
-        for(int i = 0; i <MAX_LIGHTS; i++ ){
-            location_lightPosition[i] = super.getUnifromLocation("location_lightPosition[" + i +"]");
-            location_lightColour[i] = super.getUnifromLocation("location_lightColour[" + i +"]");
+        for(int i=0;i<MAX_LIGHTS;i++){
+            location_lightPosition[i] = super.getUnifromLocation("lightPosition[" + i + "]");
+            location_lightColour[i] = super.getUnifromLocation("lightColour[" + i + "]");
         }
-
     }
 
     public void loadNumberOfRows(int numberOfRows){
         super.loadFloat(location_numberOfRows, numberOfRows);
     }
 
-    public void loadOffset(float x, float y ){
-        super.load2DVector(location_offset, new Vector2f(x, y));
+    public void loadOffset(float x, float y){
+        super.load2DVector(location_offset, new Vector2f(x,y));
     }
 
     public void loadSkyColour(float r, float g, float b){
-        super.loadVector(location_skyColour, new Vector3f(r, g, b));
+        super.loadVector(location_skyColour, new Vector3f(r,g,b));
     }
 
-    public void loadFakeLightingVariable(Boolean useFakeLighting){
-        super.loadBoolean(location_useFakeLighting, useFakeLighting);
+    public void loadFakeLightingVariable(boolean useFake){
+        super.loadBoolean(location_useFakeLighting, useFake);
     }
 
-    public void loadShineVariables(float damper, float reflectivity){
+    public void loadShineVariables(float damper,float reflectivity){
         super.loadFloat(location_shineDamper, damper);
         super.loadFloat(location_reflectivity, reflectivity);
+    }
 
+    public void loadTransformationMatrix(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix, matrix);
     }
 
     public void loadLights(List<Light> lights){
-        for(int i = 0; i<MAX_LIGHTS; i++){
+        for(int i=0;i<MAX_LIGHTS;i++){
             if(i<lights.size()){
                 super.loadVector(location_lightPosition[i], lights.get(i).getPosition());
                 super.loadVector(location_lightColour[i], lights.get(i).getColour());
@@ -97,23 +95,18 @@ public class StaticShader extends ShaderProgram {
                 super.loadVector(location_lightPosition[i], new Vector3f(0, 0, 0));
                 super.loadVector(location_lightColour[i], new Vector3f(0, 0, 0));
             }
-
         }
-    }
-
-    public void loadTransformationMatrix(Matrix4f transformation){
-        super.loadMatrix(location_transformationMatrix, transformation);
-    }
-
-    public void loadProjectionMatrix(Matrix4f projection){
-        super.loadMatrix(location_projectionMatrix, projection);
     }
 
     public void loadViewMatrix(Camera camera){
         Matrix4f viewMatrix = Maths.createViewMatrix(camera);
         super.loadMatrix(location_viewMatrix, viewMatrix);
     }
+
+    public void loadProjectionMatrix(Matrix4f projection){
+        super.loadMatrix(location_projectionMatrix, projection);
+    }
+
+
+
 }
-
-
-
